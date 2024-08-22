7f00a79363c84341e34c5b5bda1ece93a8d46cb7
Anders Wagner <awa@cvation.com>
2024-08-22 19:22:15 +0200

    Updated experiment

--- LocalityExperiment.py
+++ LocalityExperiment.py
@@ -1,11 +1,16 @@
-import numpy as np
 import matplotlib.pyplot as plt
 from BinaryPoint import *
 from LSVF import *
-
+import matplotlib.ticker as mticker
 
 if __name__ == "__main__":
     binaryPoints = []
+
+    repetitions = 5000
+    bucketExponents = [2, 4, 8]
+    bucketSizes = [2**x for x in bucketExponents]
+
+    collisionsInBucket = [[0] * 1024 for _ in bucketSizes]
 
     for i in range(1025):
         x = "0" * (1024-i)
@@ -17,33 +22,29 @@ if __name__ == "__main__":
     # The index is now how many bits are set in the binary vector
     # print(binaryPoints[512].hamDistPopCnt(binaryPoints[513]))
     #collisions = [[] for _ in range(1024)]
-    plotX = []
-    plotY = []
 
-    for _ in range(10):
-        for i in range(1, 1024, 1):
-            lsh = LSVF(binaryPoints, i, 1, 1024)
+    for i in range(len(bucketSizes)):
+        for _ in range(repetitions):
+            lsh = LSVF(binaryPoints, bucketSizes[i], 1, 1024)
             for bucketKey in lsh.buckets[0]:
                 if baselinePoint in lsh.buckets[0][bucketKey]:
                     # Do collisions on length
                     for point in lsh.buckets[0][bucketKey]:
-                        if point != baselinePoint:
-                            #collisions[i].append()
-                            plotX.append(i)
-                            plotY.append(baselinePoint.hamDistPopCnt(point))
-        #print(len(collisions[i]))
-        print(_)
+                        collisionsInBucket[i][baselinePoint.hamDistPopCnt(point)] += 1
     
-    coef = np.polyfit(plotX, plotY, 2)
-    poly = np.poly1d(coef)
+    i = 0
+    for collisionBucket in collisionsInBucket:
+        collisionBucket = [x / repetitions for x in collisionBucket]
+        plt.plot(collisionBucket, label=f'k = {bucketExponents[i]}')
+        i += 1
 
-    xCurve = np.linspace(min(plotX), max(plotX), 1024)
-    yCurve = poly(xCurve)
+    plt.xlabel("Hamming Distance")
+    plt.ylabel("Collision Probability (Logarithmic)")
+    plt.title("Collision probability for LSVF ")
 
-    plt.scatter(plotX, plotY, s=1, color='black')
-    plt.plot(xCurve, yCurve, color="red")
-    plt.title("Collisions based on distance and amount of buckets")
-    plt.xlabel("Buckets")
-    plt.ylabel("Hamming Distance")
-    plt.tight_layout()
+    plt.yscale("log")
+    plt.gca().yaxis.set_major_formatter(mticker.FormatStrFormatter('%.2f'))
+
+    plt.legend(title="Legend")
+
     plt.show()
